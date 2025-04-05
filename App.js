fbf6c7d028a59d6c1144bd7ed47d25e22f13d6ed
Narendra Kumar Sharma <Sharmanarendra417@gmail.com>
2025-04-05 14:45:58 +0530

    Episode 1

---
+++ App.js
@@ -0,0 +1,20 @@
+// const heading = React.createElement(
+//   "h1",
+//   { id: "heading" },
+//   "Hello World from React"
+// );
+
+const parents = React.createElement("div", { id: "Parent" }, [
+  React.createElement("div", { id: "Child1" }, [
+    React.createElement("h1", {}, "I am a h1 Tag"),
+    React.createElement("h2", {}, "I am a h2 Tag"),
+  ]),
+  React.createElement("div", { id: "Child2" }, [
+    React.createElement("h1", {}, "I am a h1 Tag"),
+    React.createElement("h2", {}, "I am a h2 Tag"),
+  ]),
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parents);
